f72c0d7f2438a3256a7181fb5433989bcac8d9b3
Sasha <samsiki50@gmail.com>
2023-05-08 23:09:33 +0300

    Add 4 Lab

---
+++ 26.04.2023/ЛР04/script.js
@@ -0,0 +1,15 @@
+function Show()
+{
+    var arr = ['a','b','c'];
+    alert(arr);
+}
+function ShowElements()
+{
+    var arr = ['a','b','c'];
+    alert(arr[0] + " " + arr[1] + " " + arr[2]);
+}
+function ShowString()
+{
+    arr = ['a', 'b', 'c', 'd']
+    alert(arr[0] + "+" + arr[1] + "," + arr[2] + "+" + arr[3])
+}
